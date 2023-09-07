d8f3ebc7b614ee81a38a631c56a4cb7164bb493e
Dmitrii Bazarkin <bazarkin.dmitrii@gmail.com>
2023-09-08 01:14:08 +0700

    feat: query param

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -5,11 +5,16 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
-        var app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
-        });
+        var app = Javalin.create(config -> config.plugins.enableDevLogging());
         // Описываем что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+        //app.get("/hello", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> {
+            var name = ctx.queryParam("name");
+            assert name != null;
+            var response = name.equals("John") ? "Hello, John!" : "Hello, World!";
+            ctx.result(response);
+        });
+
+        app.start(7070);
     }
 }
